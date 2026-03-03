38367f6501c5a802c95f823488a61ed4a69cc097
hiranya25 <hiranyapatil20@gmail.com>
2026-03-03 23:04:29 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,47 +4,46 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
-def main():
-    if len(sys.argv) < 2:
-        return
-    site=sys.argv[1]
-    parser=urlparse(site)
-    if not parser.scheme:
-        site= "https://"+site
-    opt = Options()
-    opt.add_argument("--headless")
-    opt.add_argument("--disable-gpu")
-    opt.add_argument("--no-sandbox")
+if len(sys.argv)< 2:
+    sys.exit()
 
-    driver=webdriver.Chrome(options=opt)
-    driver.get(site)
-    driver.implicitly_wait(5)
-    page=driver.page_source
-    driver.quit()
+site=sys.argv[1]
+parser=urlparse(site)
+if not parser.scheme:
+    site= "https://"+site
 
-    bs= BeautifulSoup(page,"html.parser")
-    if bs.title is not None:
-        print("Title:")
-        print(bs.title.text.strip())
-    else:
-        print("Title not found")
+opt=Options()
+opt.add_argument("--headless")
+opt.add_argument("--disable-gpu")
+opt.add_argument("--no-sandbox")
 
-    if bs.body is not None: 
-        bdy_txt=bs.body.get_text(separator=" ",strip=True) 
-        print("Page Body:") 
-        print(bdy_txt) 
-    else: 
-        print("Body not found")
+driver=webdriver.Chrome(options=opt)
+driver.get(site)
+driver.implicitly_wait(5)
+page=driver.page_source
+driver.quit()
 
-    all_links=[]
-    for a in bs.find_all("a"):
-        link_val=a.get("href")
-        if link_val and link_val not in all_links:
-            all_links.append(link_val)
+bs=BeautifulSoup(page, "html.parser")
 
-    print("\nAll Links:")
-    for link in all_links:
-        print(link)
+if bs.title is not None:
+    print("Title:")
+    print(bs.title.text.strip())
+else:
+    print("Title not found")
 
-if __name__== "__main__":
-    main()
+if bs.body is not None:
+    bdy_txt= bs.body.get_text(separator=" ",strip=True)
+    print("Page Body:")
+    print(bdy_txt)
+else:
+    print("Body not found")
+
+all_links=[]
+for a in bs.find_all("a"):
+    link_val=a.get("href")
+    if link_val and link_val not in all_links:
+        all_links.append(link_val)
+
+print("\nAll Links:")
+for link in all_links:
+    print(link)
