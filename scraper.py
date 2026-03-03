e20ec39b3eeff4b7952d27fa4fe2cdc77ceedc4b
hiranya25 <hiranyapatil20@gmail.com>
2026-03-03 22:52:21 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,36 +1,50 @@
 import sys
-import requests
+from urllib.parse import urlparse
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
 def main():
     if len(sys.argv) < 2:
         return
-    site= sys.argv[1]
-    res= requests.get(site)
-    page= res.text
-    bs=BeautifulSoup(page,"html.parser")
+    site=sys.argv[1]
+    parser=urlparse(site)
+    if not parser.scheme:
+        site= "https://"+site
+    opt = Options()
+    opt.add_argument("--headless")
+    opt.add_argument("--disable-gpu")
+    opt.add_argument("--no-sandbox")
+
+    driver=webdriver.Chrome(options=opt)
+    driver.get(site)
+    driver.implicitly_wait(5)
+    page=driver.page_source
+    driver.quit()
+
+    bs= BeautifulSoup(page,"html.parser")
     if bs.title is not None:
         print("Title:")
-        t= bs.title.text.strip()
-        print(t)
+        print(bs.title.text.strip())
     else:
         print("Title not found")
 
-    if bs.body is not None:
-        body_text =bs.body.get_text(separator=" ",strip=True)
-        print("Page Body:")
-        print(body_text)
-    else:
+    if bs.body is not None: 
+        bdy_txt=bs.body.get_text(separator=" ",strip=True) 
+        print("Page Body:") 
+        print(bdy_txt) 
+    else: 
         print("Body not found")
-        
-    all_link=[]
-    for a in bs.find_all("a"):
-        link_val= a.get("href")
-        if link_val is not None and link_val not in all_link:
-            all_link.append(link_val)
-    print("\nAll Links:")
-    for i in all_link:
-        print(i)
-if __name__ == "__main__":
-    main()
 
+    all_links=[]
+    for a in bs.find_all("a"):
+        link_val=a.get("href")
+        if link_val and link_val not in all_links:
+            all_links.append(link_val)
+
+    print("\nAll Links:")
+    for link in all_links:
+        print(link)
+
+if __name__== "__main__":
+    main()
